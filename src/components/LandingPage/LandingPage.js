374ce03c1809237ebdd6e15eb3f58f7ec66d2a4d
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-04-12 02:19:56 +0530

    First commit Landing page design

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandingPage = () => {
+    return (
+        <>
+            <section className='landingPageSection'>
+
+                <div className="headDiv">
+
+                    <h1>siphtor</h1>
+
+                </div>
+
+                <div className="description">
+
+                    <h4>
+                        Be a <strong>Siphtor</strong>  and start <br />
+                        a conversation
+                    </h4>
+
+                </div>
+
+                <div className="target">
+
+                    <p>
+
+                        Personalized information and simple sharing
+
+                    </p>
+
+                </div>
+
+            </section>
+
+        </>
+    )
+}
+
+export default LandingPage
